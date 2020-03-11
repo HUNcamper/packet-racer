00d4735cce5eef18e929df7f839eb5fe3392a6e2
HUNcamper <selimnahimi@gmail.com>
2020-03-11 19:27:52 +0100

    Minor changes

--- Packet Racer/Assets/Scripts/Router.cs
+++ Packet Racer/Assets/Scripts/Router.cs
@@ -6,16 +6,22 @@ using PacketRacer;
 public class Router : MonoBehaviour
 {
     private IPv4Address address_ipv4;
+    private List<NetInterface> interface_list = new List<NetInterface>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        interface_list.Add(new NetInterface(gameObject));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void HandlePacket(Packet packet)
+    {
+
     }
 }
